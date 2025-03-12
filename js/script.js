7476e95e483843e8780fb048334ec4039d2bd340
AlbertoMameli <mamelialberto2@gmail.com>
2025-03-12 18:31:28 +0100

    feat:aggiunta  stampa in pagina prezzo finale da fare migliorie prima della consegna

--- js/script.js
+++ js/script.js
@@ -30,8 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-    //creo un array con i codici sconto
-    const codiciPromo = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+    //creo un array con i codici sconto e creo una costante con le ore di lavoro da svolgere 
+    const codiciSconto = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+    const oraLavoro = 10;
 
 
 
@@ -51,15 +52,43 @@ document.addEventListener("DOMContentLoaded", function () {
         const lavoro = selectWork.value;
         const codicePromo = couponInput.value.trim();
         const accettatoPrivacy = checkPrivacy.checked; //se selezionato mi da true
-        console.log(accettatoPrivacy);
+
+        //dopo che ho recuperato i valori cosa posso fare?
+        //prima posso calcolarmi il prezzo base
         
+        const prezzoBase = prezzoLavoro [lavoro] * oraLavoro;
+
+        //ora posso aggiungere la condizione se l'utente inserisce 
+        //il codice sconto, devo controllare se viene inserito corretamente
+        //aggiungo il metodo includes per poter verificare se questo codice è inserito nell'array (true o false)
+        // se non viene inserito o non viene inserito correttamente allora
+        // il prezzofinale sara uguale al prezzo base
+
+        let sconto = 0;
+        if (codicePromo && codiciSconto.includes(codicePromo))  {
+            sconto = prezzoBase * 0.25; 
+            //stampiamo un messaggio che dice 
+            scontoOttenuto.innerText = `Codice sconto valido`;
+            
+           
+        }  else if (codicePromo) {
+            //stampiamo il messaggio che il codice non è valido
+            scontoOttenuto.innerText = `Codice sconto non valido`;
+
+        }
+        else {
+            scontoOttenuto.innerText = "";
+
+        }
 
 
-
-
-
-
-
+        // ora dopo aver calcolato lo sconto calcoliamo il prezzo finale con:
+        let prezzoFinale = prezzoBase - sconto;
+        console.log(prezzoFinale);
+        resultPrice.innerText = `${prezzoFinale.toFixed(2)}`;
+        console.log(prezzoFinale);
+        
+    
 
 
 
